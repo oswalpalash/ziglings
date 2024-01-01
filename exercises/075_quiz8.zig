cca165112927c4bd0da9a6572925bace6f7f2416
Palash Oswal <hello@oswalpalash.com>
2023-12-31 20:48:04 -0500

    Finish comptime quiz 2; sentinels

--- exercises/075_quiz8.zig
+++ exercises/075_quiz8.zig
@@ -47,9 +47,13 @@ const Path = struct {
 // time, we can perhaps shorten this a bit with a simple function
 // instead.
 //
-// Please fill in the body of this function!
 fn makePath(from: *Place, to: *Place, dist: u8) Path {
-    return Path{ .from = from, .to = to, .dist = dist };
+    var path = Path{
+        .from = comptime from,
+        .to = comptime to,
+        .dist = comptime dist,
+    };
+    return path;
 }
 
 // Using our new function, these path definitions take up considerably less
